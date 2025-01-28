f052ce25639ad1ff06cc2d4885d3def4290c1f8a
HEE082024KH <hee082024@gmail.com>
2025-01-28 11:37:27 +0100

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,16 @@
+﻿using RLNET;
+
+namespace RogueSharpV3Tutorial
+{
+    public class Game
+    {
+        // The screen height and width are in number of tiles
+        private static readonly int screenWidth = 100;
+        private static readonly int screenHeight = 70;
+
+        private static RLRootConsole rootConsole;
+        
+        public static void Main()
+        {}
+    }
+}
