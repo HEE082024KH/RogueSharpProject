7ffd7433975a561ccf88b9f7e6ec068acf8d8366
HEE082024KH <hee082024@gmail.com>
2025-01-28 14:58:51 +0100

    Added Update and Render methods

--- Game.cs
+++ Game.cs
@@ -9,8 +9,35 @@ namespace RogueSharpV3Tutorial
         private static readonly int screenHeight = 70;
 
         private static RLRootConsole rootConsole;
-        
+
         public static void Main()
-        {}
+        {
+            // This must be the exact name of the bitmap font file we are using or it will error.
+            string fontFileName = "terminal8x8.png";
+            // The title will appear at the top of the console window
+            string consoleTitle = "RougeSharp Tutorial - level 1";
+            // Tell RLNet to use the bitmap font that we specified and that each tile is 8 x 8 pixels
+            rootConsole = new RLRootConsole(fontFileName, screenWidth, screenHeight,
+                8, 8, 1f, consoleTitle);
+            // Set up a handler for RLNET's Update event
+            rootConsole.Update += OnRootConsoleUpdate;
+            // Set up a handler for RLNET's Render event
+            rootConsole.Render += OnRootConsoleRender;
+            // Begin RLNET's game loop
+            rootConsole.Run();
+            
+            // Event handler for RLNET's Update event
+            static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
+            {
+                rootConsole.Print(10, 10, "It Worked!", RLColor.White);
+            }
+            
+            // Event handler for RLNET's Render event
+            static void OnRootConsoleRender(object sender, UpdateEventArgs e)
+            {
+                // Tell RLNET to draw the console that we set
+                rootConsole.Draw();
+            }
+        }
     }
 }
